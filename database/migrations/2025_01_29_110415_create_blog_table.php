120ca19c7d874923655328b7808e4e6970ffd3ed
Wilsen <wilsenmokodaser@gmail.com>
2025-01-29 19:24:53 +0800

    add database

---
+++ database/migrations/2025_01_29_110415_create_blog_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_blog');
+            $table->string('isi_blog');
+            $table->string('tanggal_blog');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog');
+    }
+};
